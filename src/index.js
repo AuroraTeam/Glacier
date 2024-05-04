f43d5e4633f016879456a7a17b9ec179c9f8e01d
Andrew Molchanov <mr.catmanrus@gmail.com>
2024-05-05 01:55:14 +0300

    POC Two-way messaging

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const { Webview } = require("../dist/webview");
+const {
+  Worker,
+  isMainThread,
+  workerData,
+  receiveMessageOnPort,
+  parentPort,
+} = require("node:worker_threads");
 
-const w = new Webview();
-w.title("Glacier Demo");
-w.size(600, 600);
-w.html(`<html>
+if (isMainThread) {
+  const worker = new Worker(__filename, { workerData: { hello: "world" } });
+  worker.on("message", (msg) => console.log(msg));
+  worker.on("error", (err) => console.error(err));
+  worker.on("exit", (code) => console.log("Window closed, code:", code));
+  console.log("Log after start");
+  setTimeout(() => {
+    console.log("Repeat log after start");
+    worker.postMessage("Hello from Node.js");
+  }, 1000);
+} else {
+  console.log("Worker data:", workerData);
+
+  const webview_window = new Webview();
+  webview_window.title("Glacier Demo");
+  webview_window.size(800, 600);
+  webview_window.html(`<html>
   <head>
     <style>body{display: flex; align-items: center; justify-content: center; height: 100vh; font-family: sans-serif; margin: 0;}</style>
   </head>
@@ -11,4 +31,13 @@ w.html(`<html>
     <h1>Hello from Webview</h1>
   </body>
 </html>`);
-w.show();
+  webview_window.bind("__call_backend", () => {
+    let message = receiveMessageOnPort(parentPort);
+    if (!message) return;
+
+    ({ message } = message);
+    parentPort.postMessage(message);
+  });
+  webview_window.eval(`setInterval(() => __call_backend(), 4)`);
+  webview_window.show();
+}
